501eeacf481fcaaa87b33e74ed88a3e909c84cf6
Desarrollo1 <desarrollo1@Desarrollo.local>
2015-02-05 13:14:16 -0600

    Initial Commit

---
+++ descSPiPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  descSPiPhone
+//
+//  Created by Desarrollo1 on 2/5/15.
+//  Copyright (c) 2015 InkStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ descSPiPhone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  descSPiPhone
+//
+//  Created by Desarrollo1 on 2/5/15.
+//  Copyright (c) 2015 InkStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
